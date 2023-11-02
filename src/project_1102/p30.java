031ea878b4e81c2cf0c6244e38df288fd0638bc1
jack2256737 <jack2256737@gmail.com>
2023-11-02 23:27:41 +0800

    “updata”

---
+++ src/project_1102/p30.java
@@ -0,0 +1,18 @@
+package project_1102;
+
+public class p30 
+{
+public static void main(String[] args) 
+{
+		
+		int i,j;
+		for(i=0;i<5;i++) 
+		{
+			for(j=0;j<3;j++) 
+			{
+				System.out.println("i圈"+i+":j圈"+j);
+			}
+		}
+	}
+
+}
